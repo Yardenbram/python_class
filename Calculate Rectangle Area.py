71155eb1713f20d8bba9b8c12dca0ed5aa980951
Yardenbram <yardenbram@gmail.com>
2025-05-03 21:44:29 +0300

    Add files via upload

---
+++ Calculate Rectangle Area.py
@@ -0,0 +1,9 @@
+def rectangle_area(length, width):
+    return length * width
+
+print(rectangle_area(5, 10))
+print(rectangle_area(3, 7))
+print(rectangle_area(0, 9))
+print(rectangle_area(4, 4))
+print(rectangle_area(12, 2))
+ 
